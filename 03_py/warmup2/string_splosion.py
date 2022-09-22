8d9ce0f2109ed1c91acb34959bf15fe3697ef38b
Lauren Lee <llee30@stuy.edu>
2022-09-21 23:23:32 -0400

    finish wu12 and l12

--- 03_py/warmup2/string_splosion.py
+++
@@ -1,8 +0,0 @@
-# Given a non-empty string like "Code" return a string like "CCoCodCode".
-def string_splosion(str):
-    retval = ""
-    for x in range(len(str) + 1):
-        retval = retval + str[:x]
-    return retval
-
-print(string_splosion("Code"))
--- 03_py/warmup2/string_times.py
+++
@@ -1,6 +0,0 @@
-def string_times(str, n):
-  retval = ''
-  while ( n > 0):
-    retval = retval + str
-    n = n - 1
-  return retval
