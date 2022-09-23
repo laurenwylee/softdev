57ce801355391adc9d5cab60dd0662c0519e0dc9
Lauren Lee <llee30@stuy.edu>
2022-09-22 22:02:50 -0400

    edits to 04

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -1,7 +1,26 @@
 '''
 JKL: Jeremy Kwok, Kevin Li, Lauren Lee
 SoftDev
-
+Dictionaries and randomly accessing values
+2022-09-22
+time spent: .75 hours (in-class time)
+DISCO
+    - Typecasting something into an int is done by int(<thing you want to typecast>)
+    - list() can take a dictionary parameter and return a list of keys
+    - rng.choice() takes a list parameter and returns a random value in the list
+    - dict.keys and dict.values return a view object, and you can't access the values in a view object using an index (ex: krewes.keys()[0] doesn't work)
+QCC
+    - Dictionaries are kinda like 2d arrays except the other dimension can have variables of another data type
+OPS SUMMARY
+    Randint:
+    - Get a list of the keys in krewes
+    - Take the value at a random index from 0 to 2 of the list of keys
+    - Use that key to access the corresponding list and then return the devo at a random index from 0 to 1 less than the length of the list
+    Random:
+    - Same as above but using .random and typecasting instead of randint
+    Choice:
+    - use .choice on the list of keys
+    - use .choice on the list corresponding to that key
 '''
 import random as rng
 krewes = {2:['a','b','c'], 7:['y','z'], 8:['j', 'k', 'l']}
@@ -11,7 +30,7 @@ def choose():
     keys = list(krewes)
     period = rng.randint(0,len(keys))
     devo = rng.randint(0, len(krewes[period]))
-    return [krewes[period][devo]]
+    return [krewes[period][devo]] #fix the list thing
     #using random
 
 
