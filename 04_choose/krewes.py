d1d17a2e6e605564ddd1def9f9a4a4e1cab4320f
Lauren Lee <llee30@stuy.edu>
2022-09-22 21:59:21 -0400

    update

--- 04_choose/krewes.py
+++ 04_choose/krewes.py
@@ -8,15 +8,19 @@ krewes = {2:['a','b','c'], 7:['y','z'], 8:['j', 'k', 'l']}
 
 def choose():
     #using randint
-
+    keys = list(krewes)
+    period = rng.randint(0,len(keys))
+    devo = rng.randint(0, len(krewes[period]))
+    return [krewes[period][devo]]
     #using random
 
-    
+
     #using choice
-    period = rng.choice(2,7,8) #choose a random key
-    devo = rng.choice(krewes[period]) #choose a random index of the list that corresponds with the chosen key
-    return devo
+    #period = rng.choice([2,7,8]) #choose a random key
+    #devo = rng.choice(krewes[period]) #choose a random index of the list that corresponds with the chosen key
+    #return devo
 
     #return rng.choice(krewes[rng.choice([2,7,8])])
 
 print(choose())
+#print(krewes[0])
