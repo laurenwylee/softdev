21b70bba6e13e8cfd081fbeaa4fac2e626e67267
Lauren Lee <llee30@stuy.edu>
2022-10-01 00:11:44 -0400

    new algo

--- 06_py-csv/numbercruncher.py
+++ 06_py-csv/numbercruncher.py
@@ -14,27 +14,48 @@ HOW THIS SCRIPT WORKS
 '''
 import random as rng
 
-occupation = open("occupations.csv").read()
-#print(occupation)
+occupation = open("occupations.csv").read() #reading the csv file into a string
 
-occupation = occupation.split("\n")
-occupation.pop(0)
-occupation.pop(len(occupation)-1)
-occupation.pop(len(occupation)-1)
+occupation = occupation.split("\n") #split each new line 
+occupation.pop(0) #delete the heading
+occupation.pop(len(occupation)-1) #delete the extra empty line
+total = occupation.pop(len(occupation)-1) #delete the total of the values and store value
 
+#create dictionary
 jobs = {}
 for x in occupation:
-    if x.contains(", "):
-        x.split()
-    job = x.split(",")
+    job = x.rsplit(",", 1)
     jobs[job[0]] = job[1]
+    
+vals = list(jobs.values())
+#convert the values into floats
+for x in range(0, len(vals)):
+    vals[x] = float(vals[x])
 
 def choose():
-    vals = list(jobs.values())
+    #order dictionary in descending order of values
+    vals.sort(reverse = True)
+    sort_jobs = {}
     for x in vals:
-        x = float(x)
-    return rng.choices(list(jobs), weights = val)
-#print(choose())
+        for key in jobs:
+            if float(jobs[key]) == x:
+                sort_jobs[key] = x
+                del jobs[key]
+                break
 
-print(jobs)
-print(jobs.values())
+    #identify the total of all the values         
+    sum = total.split(",")
+    sum = float(sum[1])
+    #generate a random number from 1 to the total
+    random = round(rng.uniform(1,sum),1)
+    
+    #continue to subtract from largest to smallest values from the rand val until <= 0
+    #key of the last value to be subtracted is the outputed occupation!
+    ret_val = ""
+    for key in sort_jobs:
+        random = random - sort_jobs[key]
+        if random <= 0 :
+            return key
+    return ret_val
+
+print(choose())
