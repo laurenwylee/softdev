2c95c25712a6c58193784881ab8b8ec8fedf646d
Lauren Lee <llee30@stuy.edu>
2022-10-17 21:16:10 -0400

    finish k12

--- 12_flask-forms/app.py
+++ 12_flask-forms/app.py
@@ -1,6 +1,10 @@
-# Clyde 'Thluffy' Sinclair
-# SoftDev
-# Oct 2022
+'''
+Popeyes: Lauren Lee, Vivian Teo, Ian Jiang
+SoftDev
+K12 -- Take and Give
+2022-10-17
+time spent: 0.5
+ '''
 
 from flask import Flask             #facilitate flask webserving
 from flask import render_template   #facilitate jinja templating
@@ -28,7 +32,7 @@ PROTIP: Insert your own in-line comments
    understand what is going on.
 '''
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -44,7 +48,7 @@ def disp_loginpage():
     return render_template( 'login.html' )
 
 
-@app.route("/auth") #, methods=['GET', 'POST'])
+@app.route("/auth", methods=['POST'])
 def authenticate():
     print("\n\n\n")
     print("***DIAG: this Flask obj ***")
@@ -53,12 +57,18 @@ def authenticate():
     print(request)
     print("***DIAG: request.args ***")
     print(request.args)
-    print("***DIAG: request.args['username']  ***")
-    print(request.args['username'])
+    if 'username' in request.args: # error otherwise because request.args (MultiDict) could be empty
+        print("***DIAG: request.args['username']  ***") # username is like a key
+        print(request.args['username'])
+    if 'username' in request.form: 
+        print("***DIAG: request.form['username']  ***") # username is like a key
+        print(request.form['username'])
     print("***DIAG: request.headers ***")
     print(request.headers)
-    return render_template( 'response.html' , user = request.args['username'])  #response to a form submission
-
+    if 'username' in request.args: # error otherwise because request.args (MultiDict) could be empty
+        return render_template( 'response.html' , user = request.args['username'], meth = 'GET')  #response to a form submission
+    else:
+        return render_template( 'response.html' , user = request.form['username'], meth = 'POST')
 
     
 if __name__ == "__main__": #false if this file imported as module
---
+++ 12_flask-forms/readme.md
@@ -0,0 +1,19 @@
+## Popeyes: Lauren Lee, Vivian Teo, Ian Jiang
+## SoftDev
+## K12 -- Take and Give
+## 2022-10-17
+## time spent: 0.5
+
+### DISCO
+* GET request -- input in request.args
+* POST request -- input in request.form
+* Request method defaults to GET
+* Having ['GET','POST'] allows frontend to make any request they want
+* Making a POST request is done in the html file -- method = 'post'
+
+### QCC
+* When do we use GET vs POST?
+* Are there other request methods? What are the differences between them?
+
+### clearest breakdown of the differences between a GET and POST request 
+The request method defaults to GET unless the form specifies to use a POST method. GET and POST are mutually exclusive. GET corresponds with request.args so when a GET request is made, the form input is stored in request.args. POST corresponds with request.form so when a POST request is made the form input is stored in request.form. Regardless, we are able to see if a GET request is made if we see the form input in the url after submitting the form. We can also check the terminal which tells us if a GET or POST request is made.In the following app.py code, we check if request.args or request.forms contain an input to distinguish between GET and POST requests.
