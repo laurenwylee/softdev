b68c41edf56a60b51ddd40da7574c29b349f53f0
Lauren Lee <llee30@stuy.edu>
2022-11-07 14:33:54 -0500

    k19

--- 19_sessions/app.py
+++ 19_sessions/app.py
@@ -2,53 +2,66 @@
 # SoftDev
 # K19 -- Sessions Greetings
 # 2022-11-03
-# time spent: 1.5
+# time spent: 3
 
 
 
 #the conventional way:
 import re
-from flask import Flask, render_template, request
-
+from flask import Flask, render_template, request, session, redirect
+import os
 app = Flask(__name__)    #create Flask object
 
 username = "POPEYES"
 password = "chicken"
-success = True
 exception = "username and pw wrong"
+app.secret_key = os.urandom(32)
 
-@app.route("/") #, methods=['GET', 'POST'])
+@app.route("/", methods=['GET', 'POST'])
 def disp_loginpage():
-    return render_template('login.html', exception = "")  
+    if 'username' in session:
+        return render_template('response.html')
+    return render_template('login.html', message = "Type in a username and password")  
 
 
-@app.route("/auth") #, methods=['GET', 'POST'])
+@app.route("/auth", methods=['GET', 'POST'])
 def authenticate():
-    try:
+    if request.method == 'POST':
+        user = request.form['username']
+        pw = request.form['pass']
+    if request.method == 'GET':
         user = request.args['username']
         pw = request.args['pass']
-        print(user)
-        print(pw)
-        if user != username:
-            print("first if")
-            
-            print("after first if")
-            success = False
-        if pw != password:
-            print("second if")
-            
-            success = False
-        print(exception)
-        if not success:
-            print("not success")
-            return render_template('login.html', message = exception)
-        else:
-            return render_template('response.html')
-    except:
-        print("*********" + request.args['username'])
-        return render_template('login.html', message = "Please try again")
+    #pw and user correct
+    if username in  user and password in pw:
+        if request.method == 'POST':
+            session['username'] = request.form['username']
+        if request.method == 'GET':
+            session['username'] = request.args['username']
+        print(session)
+        return render_template('response.html')
+    #empty pw or user
+    if "" == user and "" == pw:
+        return render_template('login.html', message = "Please type in a username and password")
+    elif "" == user:
+        return render_template('login.html', message = "Please type in a username")
+    elif "" == pw:
+        return render_template('login.html', message = "Please type in a password")
+    #wrong pw or user
+    if user != username and pw != password:
+        return render_template('login.html', message = "Please input a correct username and password")
+    elif user != username:
+        return render_template('login.html', message = "Please input a correct username")
+    elif pw != password:
+        return render_template('login.html', message = "Please input a correct password")
+    #unidentified error
+    else:
+        return render_template('login.html', message = "unidentified")
 
-
+@app.route("/logout", methods=['GET', 'POST'])
+def logout():
+    session.pop('username')
+    return redirect('http://127.0.0.1:5000/')
   
 
 
